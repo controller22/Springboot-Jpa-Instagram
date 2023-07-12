0bde3066f74bf0deb101a27f2d9f2aaa3b963047
DH <shim5013@naver.com>
2023-07-12 10:55:36 +0900

    initial

---
+++ src/main/java/shop/mtcoding/instagram/InstagramApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramApplication.class, args);
+	}
+
+}
